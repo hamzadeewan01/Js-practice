a9f7cafde6168a6108a33c7b2031be70ff964a8b
hamzadeewan01 <102538357+hamzadeewan01@users.noreply.github.com>
2022-05-20 20:11:09 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// var a = +prompt("Enter your ist number");
+// var b = +prompt("Enter your 2nd number");
+// var opt = prompt("Enter your operater , + - * /");
+// var result;
+
+// if ( opt === "+") {
+//     result = a + b;
+// }
+
+// if ( opt === "-") {
+//     result = a - b;
+// }
+
+// if ( opt === "*") {
+//     result = a * b;
+// }
+
+// if ( opt === "/") {
+//     result = a / b;
+// }
+
+// alert(result);
